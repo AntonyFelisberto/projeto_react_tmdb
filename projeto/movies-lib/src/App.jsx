28b39b045a291469d4a8d859b1fc39340970d3b7
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-03-29 17:52:07 -0300

    inicio components

--- projeto/movies-lib/src/App.jsx
+++ projeto/movies-lib/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Link,Outlet } from "react-router-dom"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav id="navbar">
+          <h2>
+            <Link to="/">Movies Lib</Link>
+          </h2>
+          <Link to="/movie/1">Movie</Link>
+          <Link to="/search">Search</Link>
+        </nav>
+        <h2>Movies Lib</h2>
+        <Outlet/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-
-          {import.meta.env.API_KEY}
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
