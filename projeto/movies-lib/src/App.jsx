f316575b0cc17bebcd7142e2396544f287d64b08
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-03-29 13:12:25 -0300

    testes

--- projeto/movies-lib/src/App.jsx
+++ projeto/movies-lib/src/App.jsx
@@ -23,6 +23,8 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+
+          {import.meta.env.API_KEY}
         </p>
       </div>
       <p className="read-the-docs">
